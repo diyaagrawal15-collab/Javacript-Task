82f8473d743b4a4d43c338cdda466c3af7d564b6
diyaagrawal15-collab <diyaagrawal203@gmail.com>
2026-07-23 12:51:59 +0530

    Muti color pallete

--- script.js
+++ script.js
@@ -1,8 +1,33 @@
-const themeBtn = document.getElementById("themeBtn");
+const page = document.getElementById("page");
+const heading = document.getElementById("heading");
+const redbtn = document.getElementById("redbtn");
+const bluebtn = document.getElementById("bluebtn");
+const greenbtn = document.getElementById("greenbtn");
+const purplebtn = document.getElementById("purplebtn");
 
-console.log(themeBtn);
+function changecolor(color, message,){
+    page.style.backgroundColor = color;
+    heading.innerHTML = message;
+}
 
-themeBtn.addEventListener("click", function () {
-    alert("Button Clicked!");
-    document.body.classList.toggle("dark-mode");
-});
+redbtn.addEventListener('click', function(){
+    changecolor('red', 'Red selected!');
+})
+
+greenbtn.addEventListener('click', function(){
+    changecolor('green', 'Green selected!');
+})
+
+bluebtn.addEventListener('click', function(){
+    changecolor('blue', 'Blue selected!');
+})
+
+purplebtn.addEventListener('click', function(){
+    changecolor('purple', 'purple selected!');
+})
+
+const resetbtn = document.getElementById('resetbtn');
+
+resetbtn.addEventListener('click', function(){
+    changecolor('white', 'color-palette!',);
+})
