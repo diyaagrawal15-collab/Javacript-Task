168f5aee864053d482e9e5eab624fcd3f084b335
diyaagrawal15-collab <diyaagrawal203@gmail.com>
2026-07-23 13:08:51 +0530

    theme switcher

--- script.js
+++ script.js
@@ -1,33 +1,8 @@
-const page = document.getElementById("page");
-const heading = document.getElementById("heading");
-const redbtn = document.getElementById("redbtn");
-const bluebtn = document.getElementById("bluebtn");
-const greenbtn = document.getElementById("greenbtn");
-const purplebtn = document.getElementById("purplebtn");
+const themeBtn = document.getElementById("themeBtn");
 
-function changecolor(color, message,){
-    page.style.backgroundColor = color;
-    heading.innerHTML = message;
-}
+console.log(themeBtn);
 
-redbtn.addEventListener('click', function(){
-    changecolor('red', 'Red selected!');
-})
-
-greenbtn.addEventListener('click', function(){
-    changecolor('green', 'Green selected!');
-})
-
-bluebtn.addEventListener('click', function(){
-    changecolor('blue', 'Blue selected!');
-})
-
-purplebtn.addEventListener('click', function(){
-    changecolor('purple', 'purple selected!');
-})
-
-const resetbtn = document.getElementById('resetbtn');
-
-resetbtn.addEventListener('click', function(){
-    changecolor('white', 'color-palette!',);
-})
+themeBtn.addEventListener("click", function () {
+    alert("Button Clicked!");
+    document.body.classList.toggle("dark-mode");
+});
